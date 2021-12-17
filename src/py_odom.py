ffbe2f5ff9fb4217a1d9098a3abdf0ce0c559348
phosha_pc <kdrvpavel@gmail.com>
2021-12-18 00:33:07 +0200

    Добавлено измерение скорости

--- src/py_odom.py
+++ src/py_odom.py
@@ -13,22 +13,42 @@ from std_msgs.msg import Float32
 
 wheel_c = 210
 tpm = 1714.28
-base_wight = 0.19
+base_width = 0.19
 
 
 r_ticks = 0
 l_ticks = 0
 imu_ticks = 0
 
+
+r_dist = 0
+l_dist = 0
+base_angl = 0
+base_dist = 0
+
+Rddist = 0
+Lddist = 0
+Rold_dist = 0
+Lold_dist = 0
+
+vel_r = 0
+vel_l = 0
+
 def r_wheel_callback(data):
-    global r_ticks
+    global r_ticks, r_dist, Rddist, Rold_dist, vel_r
     r_ticks = data.data
-    #rospy.loginfo("I heard %s ticks from r_wheel",data.data)
+    r_dist = r_ticks/tpm
+    Rddist = r_dist - Rold_dist
+    Rold_dist = r_dist
+    vel_r = Rddist / 0.05
 
 def l_wheel_callback(data):
-    global l_ticks
+    global l_ticks, l_dist, Lddist, Lold_dist, vel_l
     l_ticks = data.data
-    #rospy.loginfo("I heard %s ticks from l_wheel",data.data)
+    l_dist = l_ticks/tpm
+    Lddist = l_dist - Lold_dist
+    Lold_dist = l_dist
+    vel_l = Rddist / 0.05
 
 def imu_callback(data):
     global imu_ticks
@@ -40,14 +60,16 @@ rospy.Subscriber("r_wheel_node", Int32, r_wheel_callback)
 rospy.Subscriber("l_wheel_node", Int32, l_wheel_callback)
 rospy.Subscriber("imu_z_node", Float32, imu_callback)
 
+r_speed_pub = rospy.Publisher("r_speed", Float32,queue_size=50)
+l_speed_pub = rospy.Publisher("l_speed", Float32,queue_size=50)
+
+
 odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
 odom_broadcaster = tf.TransformBroadcaster()
 
 
-r_dist = 0
-l_dist = 0
-base_angl = 0
-base_dist = 0
+
+
 
 x = 0
 y = 0
@@ -60,20 +82,28 @@ vth = 0.1
 current_time = rospy.Time.now()
 last_time = rospy.Time.now()
 
-r = rospy.Rate(100.0)
+r = rospy.Rate(50.0)
 while not rospy.is_shutdown():
     current_time = rospy.Time.now()
     dt = current_time - last_time
     odom_quat = tf.transformations.quaternion_from_euler(0, 0, imu_ticks)
     
-    r_dist = float(r_ticks/tpm)
-    l_dist = float(l_ticks/tpm)
+    
+    
 
-    base_angl =  (r_dist -l_dist) / base_wight
+    base_angl =  (r_dist -l_dist) / base_width
     base_dist = (r_dist + l_dist) / 2
 
     x = base_dist * math.cos(base_angl)
     y = base_dist * math.sin(base_angl)
+
+
+    r_speed_pub.publish(vel_r)
+
+   
+
+    rospy.loginfo(vel_r)
+    #rospy.loginfo(vel_l)
 
     odom_broadcaster.sendTransform(
         (x, y, 0.),
